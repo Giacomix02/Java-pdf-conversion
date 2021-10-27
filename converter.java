c493bc770290506ef7f5fe92cb4b7b64543abee6
giacomix02 <paoloccigiacomo@gmail.com>
2021-10-27 12:06:18 +0200

    choose format conversion

---
+++ converter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class converter{
+    public static void main(String[] argv) {
+
+        int input;
+
+        System.out.println("From pdf to... (press the number)");
+        System.out.println("1. PNG\n2.JPG");
+        System.out.print(">>");
+
+        Scanner in = new Scanner(System.in);
+        input = in.nextInt();
+
+        switch(input){
+            case 1:     //conversion to png
+            System.out.println("to PNG");
+            break;
+
+            case 2:     //conversion to jpg
+            System.out.println("to JPG");
+            break;
+
+        }
+        
+    }
+}
